c7b1816d2bc4a21beb3b369b4da95707545a7381
James <jamesmbrightman@gmail.com>
2020-10-04 12:25:28 +0100

    Updates

---
+++ precache-manifest.d7aa697ac2ec6bb27695a486fdf8c30f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cbf0a9a7413ec27b9b11d8552a3dd75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ff32f757458a754bce8",
+    "url": "/static/js/2.8dcd9df1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8dcd9df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d14261c5c93f099253d7",
+    "url": "/static/js/main.068fa243.chunk.js"
+  },
+  {
+    "revision": "5570eba6b7203dfb673f",
+    "url": "/static/js/runtime-main.99ce2d44.js"
+  }
+]);
