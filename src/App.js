5fc80558c979bdcfc461e5373d2eb87cbde001f4
James <jamesmbrightman@gmail.com>
2020-10-04 14:00:15 +0100

    Main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './App.css';
 
 function App() {
-  return (
-    <div>
-      Hello
-    </div>
-  );
+    return (
+        <>
+            <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Hammersmith+One"/>
+            <div className="main">
+                Yes.
+            </div>
+        </>
+    );
 }
 
 export default App;
